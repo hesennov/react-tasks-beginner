76b18eec6ccf433955eb2221fa7742cdc4264688
hesennov <hasanhasanov345@gmail.com>
2023-04-06 17:28:50 +0400

    task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
-import React, { useState } from "react";
-import Header from "./Component/Header";
-import "./App.css";
-import Form from "./Component/Form";
-
-const App = ({ input, setInput, todos, setTodos, setEditTodo }) => {
+const App = () => {
   return (
-    <div className="container">
-      <div className="app-wrapper">
-        <div>
-          <Header />
-        </div>
-        <div>
-          <Form
-            input={input}
-            setInput={setInput}
-            todos={todos}
-            setTodos={setTodos}
-            setEditTodo={setEditTodo}
-          />
-        </div>
-        <div></div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+    <div>App</div>
+  )
+}
+export default App
